d43a195982fbe17340b15016cdb23a4e6cf6bf31
LSuazo1 <luissuazo80@yahoo.com>
2022-07-17 21:39:08 -0600

    Componente Personaje DBZ

---
+++ src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html',
+  styles: [
+  ]
+})
+export class PersonajesComponent {
+
+
+
+}
